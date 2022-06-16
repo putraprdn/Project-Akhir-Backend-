d67fe4f728b4383010a5ae90d44ffd3253fdd14a
ajierzaldi7 <ajiemuhammad2@gmail.com>
2022-06-16 20:58:39 +0700

    add product controller

--- config/routes.js
+++ config/routes.js
@@ -7,26 +7,43 @@ const apiRouter = express.Router();
 /** Mount GET / handler */
 appRouter.get("/", controllers.main.index);
 
-/**
- * TODO: Implement your own API
- *       implementations
+/** 
+ * Implement Product API
  */
-apiRouter.get("/api/v1/posts", controllers.api.v1.post.list);
-apiRouter.post("/api/v1/posts", controllers.api.v1.post.create);
-apiRouter.put(
-  "/api/v1/posts/:id",
-  controllers.api.v1.post.setPost,
-  controllers.api.v1.post.update
-);
+
+// Product List
+apiRouter.get("/api/v1/products", controllers.api.v1.product.list);
+// Show a product by id
 apiRouter.get(
-  "/api/v1/posts/:id",
-  controllers.api.v1.post.setPost,
-  controllers.api.v1.post.show
+  "/api/v1/products/:id",
+  controllers.api.v1.product.setProduct,
+  controllers.api.v1.product.show
 );
+// Show a product by name
+apiRouter.get(
+  "/api/v1/products/name/:name",
+  controllers.api.v1.product.setProduct,
+  controllers.api.v1.product.showByName
+);
+// Show a product by category
+apiRouter.get(
+  "/api/v1/products/category/:categoryId",
+  controllers.api.v1.product.setProduct,
+  controllers.api.v1.product.showByCategory
+);
+// Create a new product
+apiRouter.post("/api/v1/products", controllers.api.v1.product.create);
+// Update a product
+apiRouter.put(
+  "/api/v1/products/:id",
+  controllers.api.v1.product.setProduct,
+  controllers.api.v1.product.update
+);
+// Delete a product
 apiRouter.delete(
-  "/api/v1/posts/:id",
-  controllers.api.v1.post.setPost,
-  controllers.api.v1.post.destroy
+  "/api/v1/products/:id",
+  controllers.api.v1.product.setProduct,
+  controllers.api.v1.product.destroy
 );
 
 module.exports = appRouter;
