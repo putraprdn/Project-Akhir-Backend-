f742926922160f60ee9ad362aad273d2c88430d3
ajierzaldi7 <ajiemuhammad2@gmail.com>
2022-06-17 20:11:16 +0700

    feat(ajie): update product controller

--- app/controllers/api/v1/productController.js
+++ app/controllers/api/v1/productController.js
@@ -5,18 +5,39 @@ module.exports = {
     List: async (req, res) => {
         try {
             const products = await model.product.findAll();
-            res.status(200).json(products);
+
+            return res.status(200).json({
+                "success": true,
+                "error": 0,
+                "message": "Products listed",
+                "data": products
+            });
         } catch (error) {
-            res.status(500).json(error);
+            res.status(500).json({
+                "success": false,
+                "error": error.code,
+                "message": error,
+                "data": null
+            });
         }
     },
     // Show a product by id
     findById: async (req, res) => {
         try {
             const product = await model.product.findByPk(req.params.id);
-            res.status(200).json(product);
+            return res.status(200).json({
+                "success": true,
+                "error": 0,
+                "message": "Product found",
+                "data": product
+            });
         } catch (error) {
-            res.status(500).json(error);
+            res.status(500).json({
+                "success": false,
+                "error": error.code,
+                "message": error,
+                "data": null
+            });
         }
     },
     // Show a product by name
@@ -27,9 +48,19 @@ module.exports = {
                     name: req.params.name
                 }
             });
-            res.status(200).json(product);
+            res.status(200).json({
+                "success": true,
+                "error": 0,
+                "message": "Product found",
+                "data": product
+            });
         } catch (error) {
-            res.status(500).json(error);
+            res.status(500).json({
+                "success": false,
+                "error": error.code,
+                "message": error,
+                "data": null
+            });
         }
     },
     // Show a product by category
@@ -40,18 +71,38 @@ module.exports = {
                     categoryId: req.params
                 }
             });
-            res.status(200).json(product);
+            res.status(200).json({
+                "success": true,
+                "error": 0,
+                "message": "Product found",
+                "data": product
+            });
         } catch (error) {
-            res.status(500).json(error);
+            res.status(500).json({
+                "success": false,
+                "error": error.code,
+                "message": error,
+                "data": null
+            });
         }
     },
     // Create a new product
     create: async (req, res) => {
         try {
             const product = await model.product.create(req.body);
-            res.status(200).json(product);
+            res.status(200).json({
+                "success": true,
+                "error": 0,
+                "message": "Product created",
+                "data": product
+            });
         } catch (error) {
-            res.status(500).json(error);
+            res.status(500).json({
+                "success": false,
+                "error": error.code,
+                "message": error,
+                "data": null
+            });
         }
     },
     // Update a product
@@ -62,9 +113,19 @@ module.exports = {
                     id: req.params.id
                 }
             });
-            res.status(200).json(product);
+            res.status(200).json({
+                "success": true,
+                "error": 0,
+                "message": "Product updated",
+                "data": product
+            });
         } catch (error) {
-            res.status(500).json(error);
+            res.status(500).json({
+                "success": false,
+                "error": error.code,
+                "message": error,
+                "data": null
+            });
         }
     },
     // Delete a product
@@ -75,9 +136,19 @@ module.exports = {
                     id: req.params.id
                 }
             });
-            res.status(200).json(product);
+            res.status(200).json({
+                "success": true,
+                "error": 0,
+                "message": "Product deleted",
+                "data": product
+            });
         } catch (error) {
-            res.status(500).json(error);
+            res.status(500).json({
+                "success": false,
+                "error": error.code,
+                "message": error,
+                "data": null
+            });
         }
     }
 }
