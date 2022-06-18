5bb64f362aab6ba4dbc0889a276307682a02bec9
putraperdana138 <80741806+putraprdn@users.noreply.github.com>
2022-06-18 15:20:26 +0700

    feat[putra]: add api base path & documentation

--- config/routes.js
+++ config/routes.js
@@ -1,59 +1,87 @@
 const express = require("express");
 const controllers = require("../app/controllers");
 
-const appRouter = express.Router();
+// Some dependencies for api documenations
+const YAML = require("yamljs");
+const swaggerUI = require("swagger-ui-express");
+const apiDocs = YAML.load("./api-doc.yaml");
+
+// const appRouter = express.Router();
 const apiRouter = express.Router();
 
-/** Mount GET / handler */
-// appRouter.get("/", controllers.main.index);
-
-appRouter.get("/category/list", controllers.api.v1.categoryController.list)
-appRouter.get("/category/list/:id", controllers.api.v1.categoryController.findById)
-appRouter.post("/category/create", controllers.api.v1.categoryController.create)
-appRouter.put("/category/update", controllers.api.v1.categoryController.update)
-appRouter.delete("/category/delete/:id", controllers.api.v1.categoryController.destroy)
-/** 
- * Implement Product API
+/**
+ * api documentation endpoint using swagger ui
  */
+apiRouter.use("/api/documentation", swaggerUI.serve, swaggerUI.setup(apiDocs));
 
-// Product List
-appRouter.get("/v1/products", controllers.api.v1.productController.list);
-// Show a product by id
-appRouter.get(
-  "/v1/products/:id",
-  controllers.api.v1.productController.findById
+/**
+ * CATEGORY API
+ */
+apiRouter.get("/api/category/list", controllers.api.v1.categoryController.list);
+apiRouter.get(
+	"/api/category/:id",
+	controllers.api.v1.categoryController.findById
 );
-// Show a product by name
-appRouter.get(
-  "/v1/products/name/:name",
-  controllers.api.v1.productController.findByName
+apiRouter.post(
+	"/api/category/create",
+	controllers.api.v1.categoryController.create
 );
-// Show a product by category
-appRouter.get(
-  "/v1/products/category/:categoryId",
-  controllers.api.v1.productController.findByCategory
+apiRouter.put(
+	"/api/category/update",
+	controllers.api.v1.categoryController.update
 );
-// Create a new product
-appRouter.post("/v1/products", controllers.api.v1.productController.create);
-// Update a product
-appRouter.put(
-  "/v1/products/:id",
-  controllers.api.v1.productController.update
-);
-// Delete a product
-appRouter.delete(
-  "/v1/products/:id",
-  controllers.api.v1.productController.destroy
+apiRouter.delete(
+	"/api/category/delete/:id",
+	controllers.api.v1.categoryController.destroy
 );
 
 /**
- * TODO: Implement your own API
- *       implementations
+ * PRODUCT API
  */
 
-appRouter.post("/user/register", controllers.api.v1.userController.register);
-appRouter.post("/user/login", controllers.api.v1.userController.login);
-appRouter.put("/user/update/:id", controllers.api.v1.userController.update);
-appRouter.delete("/user/destroy/:id", controllers.api.v1.userController.destroy);
+// Product List
+apiRouter.get("/api/product/list", controllers.api.v1.productController.list);
+// Show a product by id
+apiRouter.get(
+	"/api/product/:id",
+	controllers.api.v1.productController.findById
+);
+// Show a product by name
+apiRouter.get(
+	"/api/product/name/:name",
+	controllers.api.v1.productController.findByName
+);
+// Show a product by category
+apiRouter.get(
+	"/api/product/by-category/:categoryId",
+	controllers.api.v1.productController.findByCategory
+);
+// Create a new product
+apiRouter.post("/api/product", controllers.api.v1.productController.create);
+// Update a product
+apiRouter.put(
+	"/api/product/update/:id",
+	controllers.api.v1.productController.update
+);
+// Delete a product
+apiRouter.delete(
+	"/api/product/delete/:id",
+	controllers.api.v1.productController.destroy
+);
 
-module.exports = appRouter;
+/**
+ * USER API
+ */
+
+apiRouter.post(
+	"/api/user/register",
+	controllers.api.v1.userController.register
+);
+apiRouter.post("/api/user/login", controllers.api.v1.userController.login);
+apiRouter.put("/api/user/update/:id", controllers.api.v1.userController.update);
+apiRouter.delete(
+	"/api/user/delete/:id",
+	controllers.api.v1.userController.destroy
+);
+
+module.exports = apiRouter;
