c859f4b376e2df90653d8183d5296d01b54e743c
putraperdana138 <80741806+putraprdn@users.noreply.github.com>
2022-06-22 21:54:19 +0700

    feat[putra]: create offerhistory model, migrations, and its relations

---
+++ db/migrations/20220622140517-create-offerhistory.js
@@ -0,0 +1,48 @@
+"use strict";
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("offerhistories", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			productId: {
+				type: Sequelize.INTEGER,
+				references: {
+					model: {
+						tableName: "products",
+					},
+					key: "id",
+				},
+			},
+			createdBy: {
+				type: Sequelize.INTEGER,
+				references: {
+					model: {
+						tableName: "users",
+					},
+					key: "id",
+				},
+			},
+			price: {
+				type: Sequelize.INTEGER,
+			},
+			status: {
+				type: Sequelize.STRING,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable("offerhistories");
+	},
+};
