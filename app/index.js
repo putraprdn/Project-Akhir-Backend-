6ef3f68b8892a2ad05c9e11df6296b01f71bbebf
putraperdana138 <80741806+putraprdn@users.noreply.github.com>
2022-06-28 21:52:56 +0700

    fix[putra]: fix cors error in production

--- app/index.js
+++ app/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors')
 const router = require("../config/routes");
 
 const app = express();
 
 /** Install request logger */
 app.use(morgan("dev"));
+
+/** Install cors */
+app.use(cors());
 
 /** Install JSON request parser */
 app.use(express.json());
