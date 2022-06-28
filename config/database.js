47fcabf44337a88ad57d39231ceb9a3b1c7f05d9
DECode.studio <decode.std@gmail.com>
2022-06-28 19:50:46 +0700

    make it better

--- config/database.js
+++ config/database.js
@@ -16,10 +16,20 @@ module.exports = {
 		dialect: process.env.DB_DIALECT,
 	},
 	production: {
-		username: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME,
-		host: process.env.DB_HOST,
-		dialect: process.env.DB_DIALECT,
-	},
+		username: "bewfxrrnuxltnw",
+		password: "b9b9a160afd9c58a5808234acd12cac44569cb7a8cac60cddf70548e2de59795",
+		database: "d9pmr5vs2g0r1t",
+		host: "ec2-44-198-82-71.compute-1.amazonaws.com",
+		dialect: "postgres",
+		dialectOptions: {
+			ssl: { rejectUnauthorized: false }
+		}
+	}
+	// production: {
+	// 	username: process.env.DB_USERNAME,
+	// 	password: process.env.DB_PASSWORD,
+	// 	database: process.env.DB_NAME,
+	// 	host: process.env.DB_HOST,
+	// 	dialect: process.env.DB_DIALECT,
+	// },
 };
